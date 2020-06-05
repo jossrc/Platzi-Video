80d05a61012b3c7fc369b2096686e0fdef5937d1
Jhoss <kuro.rc10@gmail.com>
2020-06-05 10:16:42 -0500

    Creando la estructura y aplicando estilos al Footer

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -7,6 +7,7 @@ import Search from '../components/Search';
 import Categories from '../components/Categories';
 import Carousel from '../components/Carousel';
 import CarouselItem from '../components/CarouselItem';
+import Footer from '../components/Footer';
 
 const App = () => (
   <div className='App'>
@@ -22,6 +23,7 @@ const App = () => (
         <CarouselItem />
       </Carousel>
     </Categories>
+    <Footer />
   </div>
 );
 
